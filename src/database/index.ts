841ed976d10e8d4a68b8c822a438ebc61fde790c
Abmaellf <abmael.ninha@gmail.com>
2022-12-05 23:29:53 -0400

    3-Criando container do postgres

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,24 @@
-import { createConnection } from "typeorm";
+/* eslint-disable prettier/prettier */
+// import { createConnection } from "typeorm";
+// createConnection();
+import { DataSource } from "typeorm";
 
+import { Category } from "../modules/cars/model/Category";
+import { Specification } from "../modules/cars/model/Specification";
 
+export const dataSource = new DataSource({
+  type:"postgres",
+  port:5432,
+  username:"abmael",
+  password: "abmael",
+  database:"rentx",
+  entities:[
+    Category,
+    Specification
+  ],
+  migrations:[
 
-createConnection();
+  ]
+
+});
 
